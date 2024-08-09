2d80bb30351bdab4d106ebf800d9b6931b0215fa
Gabrieleabu23 <kaidgabriele23@gmail.com>
2024-08-09 15:38:20 +0200

    style : start blade dashboard

--- back-end/resources/views/dashboard.blade.php
+++ back-end/resources/views/dashboard.blade.php
@@ -3,13 +3,7 @@
 @section('content')
     <div class="container">
 
-        <h1 class="travel-app">
-            <span class="part1">TRA</span><span class="part2">VEL</span><span class="dash">-</span><span
-                class="part3">APP</span>
-        </h1>
-
-
-        <h2 class="fs-4 text-secondary my-4">
+        {{-- <h2 class="fs-4 text-secondary my-4">
             {{ __('Dashboard') }}
         </h2>
         <div class="row justify-content-center">
@@ -28,7 +22,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> --}}
     </div>
 @endsection
 
