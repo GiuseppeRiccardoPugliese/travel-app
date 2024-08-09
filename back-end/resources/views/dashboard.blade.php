03943938a0fb102ffcdf31a78e2462adac999ca8
Gabrieleabu23 <kaidgabriele23@gmail.com>
2024-08-09 14:59:34 +0200

    add: modified route welcome in dashboard & created logo.v1

--- back-end/resources/views/dashboard.blade.php
+++ back-end/resources/views/dashboard.blade.php
@@ -1,26 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h2 class="fs-4 text-secondary my-4">
-        {{ __('Dashboard') }}
-    </h2>
-    <div class="row justify-content-center">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
+    <div class="container">
 
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
+        <h1 class="travel-app">
+            <span class="part1">TRA</span><span class="part2">VEL</span><span class="dash">-</span><span
+                class="part3">APP</span>
+        </h1>
+
+
+        <h2 class="fs-4 text-secondary my-4">
+            {{ __('Dashboard') }}
+        </h2>
+        <div class="row justify-content-center">
+            <div class="col">
+                <div class="card">
+                    <div class="card-header">{{ __('User Dashboard') }}</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        {{ __('You are logged in!') }}
                     </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
+
