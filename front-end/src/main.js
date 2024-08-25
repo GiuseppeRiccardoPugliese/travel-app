33c95b197755977dffe6c0864786fe7e793736cf
GiuseppeRiccardoPugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-08-25 17:53:49 +0200

    Set-Up generico per il front-end in Vue

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
-import './style.css'
+
+//Stile General
+import './style/style.scss'
+
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
 import App from './App.vue'
 
 createApp(App).mount('#app')
