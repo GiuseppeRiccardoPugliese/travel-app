4a632135fdf99fe8f5e335e283691006e7eed075
GiuseppeRiccardoPugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-08-31 14:31:11 +0200

    Add: Arrow che ti riporta in cima alla pagina dopo almeno uno scroll Y di 100px

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -10,11 +10,29 @@ export default {
   },
   data() {
     return {
-
+      showScrollTop: false,
     }
   },
   methods: {
-
+    checkScroll() {
+      // Se la pagina e' stata scrollata di 100px allora:
+      this.showScrollTop = window.scrollY > 100;
+      //Log "true" di showScrollTop
+    },
+    scrollToTop() {
+      // Funzione per far scrollare la pagina verso l'alto
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth', //Scrolla top lentamente 'smooth' invece di essere istantaneo
+      });
+    },
+  },
+  mounted() {
+    window.addEventListener('scroll', this.checkScroll);
+  },
+  beforeDestroy() {
+    // Memory leaks
+    window.removeEventListener('scroll', this.checkScroll);
   },
 }
 
@@ -36,8 +54,38 @@ export default {
 
   </div>
 
+
+  <!-- Freccia che compare quando scrolli di 100px -->
+  <div v-if="showScrollTop" @click="scrollToTop" class="scroll-top-arrow show">
+    <i class="fa-solid fa-circle-up"></i>
+  </div>
 </template>
 
 <style lang="scss">
 @use './style/style.scss' as *;
+
+/* Stili per la freccia */
+.scroll-top-arrow {
+  position: fixed;
+  bottom: 100px;
+  right: 20px;
+  font-size: 30px;
+  cursor: pointer;
+  display: none;
+  transition: opacity 0.3s ease;
+
+  .fa-circle-up {
+    color: white;
+  }
+}
+
+/* Stili per la freccia quando visibile */
+.scroll-top-arrow.show {
+  display: block;
+  opacity: 1;
+  padding: 0px 12px;
+  background-color: #61aafd;
+  border-radius: 5px;
+  z-index: 1000;
+}
 </style>
